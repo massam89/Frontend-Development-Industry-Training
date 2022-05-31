76b0333460c7a7069651f50e4a359ddc5cb69448
Masoud <mas.sam1368@gmail.com>
2022-05-31 14:40:33 +0430

    finish assignement four

---
+++ week3/assingnments/4/js/app.js
@@ -0,0 +1,56 @@
+function computerPlay() {
+    const options = ['Paper', 'Rock', 'Scissors']
+    const randomNum = Math.floor(Math.random() * options.length)
+
+    return options[randomNum]
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if((playerSelection === 'rock' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'paper')) {
+        return 'Draw'
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You lose! scissors beat paper'
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You win! paper beat rock'
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You lose! paper beat rock'
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You win! rock beat scissors'
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You win! scissors beat paper'
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You lose! rock beat scissors'
+    } else {
+        return 'Wrong input'
+    }
+
+}
+
+(function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for(let i = 0 ; i < 5 ; i++) {
+        const playerSelection = prompt('Rock, scissors or paper?');
+        const computerSelection = computerPlay();
+        const roundMessage = playRound(playerSelection, computerSelection)
+
+        console.log(roundMessage)
+
+        if(roundMessage === 'Wrong input') {
+            i--;
+        } else if (roundMessage === 'Draw'){
+            
+        } else if (roundMessage.search('win') === -1) {
+            computerScore++
+        } else if (roundMessage.search('lose') === -1){
+            playerScore++
+        }
+    }
+
+    console.log({playerScore, computerScore})
+    
+})();
